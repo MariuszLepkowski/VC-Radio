81c0ce0bc7122c535aa5dcbc7136ef75fc6ea57d
Mariusz <mariusz.lepkowski23@gmail.com>
2023-12-29 17:37:32 +0100

    Add __pycache__/ discography.csv to .gitignore

---
+++ discography_manager.py
@@ -0,0 +1,33 @@
+import requests as r
+import pandas
+
+
+def download_discography():
+    """Downloads discography from vinniecolaiuta.com as json file and writes it into csv file."""
+
+    page_number = 0
+    page_has_content = True
+
+    while page_has_content:
+        page_number += 1
+        response = r.get(f"https://vinniecolaiuta.com/Home/GetPageOfRecordings?page={page_number}")
+        response.raise_for_status()
+        data = response.json()
+        if len(data) != 0:
+            page = pandas.DataFrame(data).drop(columns=["$id", "id", "year", "url"]).to_csv(index=False)
+            with open("discography.csv", mode="a", encoding="utf-8") as file:
+                file.write(page)
+        else:
+            page_has_content = False
+
+
+def pick_random_album():
+    """Picks random album from the discography inside discography.cdv"""
+
+    try:
+        data = pandas.read_csv("discography.csv")
+    except FileNotFoundError:
+        download_discography()
+    else:
+        random_album = data.sample()
+        print(random_album)
