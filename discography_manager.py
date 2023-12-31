0978aba8c783db4e30dbcc47376873b4691be88a
Mariusz <mariusz.lepkowski23@gmail.com>
2023-12-31 19:51:14 +0100

    mprove organization and naming in discography-related functions.

--- discography_manager.py
+++ discography_manager.py
@@ -1,36 +1,50 @@
 import requests as r
-import pandas
+import pandas as pd
 import os
 
+DISCOGRAPHY_URL = "https://vinniecolaiuta.com/Home/GetPageOfRecordings?page="
 
-def download_discography():
-    """Downloads discography as json file and writes it into csv file."""
+
+def download_page_of_discography(page_number):
+    """Downloads given page of the whole discography as json data."""
+
+    response = r.get(f"{DISCOGRAPHY_URL}{page_number}")
+    response.raise_for_status()
+    return response.json()
+
+
+def save_json_to_csv(json_data, filename='discography.csv'):
+    """Saves json_data to a CSV file called 'discography.csv'."""
+
+    if not os.path.exists(filename):
+        pd.DataFrame(json_data).drop(columns=["$id", "id", "year", "url"]).to_csv(filename, index=False)
+
+    page = pd.DataFrame(json_data).drop(columns=["$id", "id", "year", "url"])
+    page.to_csv(filename, mode="a", index=False, header=False)
+
+
+def download_and_save_whole_discography():
+    """Downloads all pages of the discography and saves it to a CSV file."""
 
     page_number = 0
     page_has_content = True
 
     while page_has_content:
         page_number += 1
-        response = r.get(f"https://vinniecolaiuta.com/Home/GetPageOfRecordings?page={page_number}")
-        response.raise_for_status()
-        data = response.json()
+        data = download_page_of_discography(page_number)
+
         if len(data) != 0:
-            page = pandas.DataFrame(data).drop(columns=["$id", "id", "year", "url"]).to_csv(index=False)
-            with open("discography.csv", mode="a", encoding="utf-8") as file:
-                file.write(page)
+            save_json_to_csv(json_data=data)
         else:
             page_has_content = False
-
 
 
 def pick_random_album():
     """Picks random album from the discography inside discography.csv"""
 
-    if os.path.exists("discography.csv"):
-        data = pandas.read_csv("discography.csv")
-    else:
-        download_discography()
-        data = pandas.read_csv("discography.csv")
+    if not os.path.exists("discography.csv"):
+        download_and_save_whole_discography()
 
+    data = pd.read_csv("discography.csv")
     random_album = data.sample()
     print(random_album)
