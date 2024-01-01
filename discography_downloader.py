d8dddbb06e09eb5dbed73910db94970267dfb774
Mariusz <mariusz.lepkowski23@gmail.com>
2024-01-01 14:23:58 +0100

    Refactor discography_downloader module to use object-oriented design
    Transform procedural code to object-oriented structure. Introduce DiscographyDownloader class for better encapsulation and maintainability. Improve code organization and readability.

---
+++ discography_downloader.py
@@ -0,0 +1,45 @@
+import requests
+import pandas as pd
+import os
+
+
+class DiscographyDownloader:
+    def __init__(self, base_url):
+        self.base_url = base_url
+        self.page_number = 0
+        self.page_has_content = True
+        self.filename = 'discography.csv'
+
+    def download_page(self, page_number):
+        """Downloads given page of the whole discography as json data."""
+        response = requests.get(f"{self.base_url}{page_number}")
+        response.raise_for_status()
+        return response.json()
+
+    def save_to_csv(self, json_data):
+        """Saves json_data to a CSV file called 'discography.csv'."""
+        if not os.path.exists(self.filename):
+            pd.DataFrame(json_data).drop(columns=["$id", "id", "year", "url"]).to_csv(self.filename, index=False)
+        else:
+            page = pd.DataFrame(json_data).drop(columns=["$id", "id", "year", "url"])
+            page.to_csv(self.filename, mode="a", index=False, header=False)
+
+    def download_and_save_whole_discography(self):
+        """Downloads all pages of the discography and saves it to a CSV file."""
+        while self.page_has_content:
+            self.page_number += 1
+            data = self.download_page(self.page_number)
+
+            if len(data) != 0:
+                self.save_to_csv(data)
+            else:
+                self.page_has_content = False
+
+    def pick_random_album(self):
+        """Picks random album from the discography inside discography.csv"""
+        if not os.path.exists(self.filename):
+            self.download_and_save_whole_discography()
+
+        data = pd.read_csv(self.filename)
+        random_album = data.sample()
+        print(random_album)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from discography_manager import *
+from discography_downloader import DiscographyDownloader
+
+DISCOGRAPHY_URL = "https://vinniecolaiuta.com/Home/GetPageOfRecordings?page="
 
 
 def main():
-    pick_random_album()
+    discography_downloader = DiscographyDownloader(DISCOGRAPHY_URL)
+
+    discography_downloader.pick_random_album()
 
 
 if __name__ == "__main__":
