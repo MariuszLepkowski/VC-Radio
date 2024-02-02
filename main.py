36022b83210feb80d900cf4594a2043cee08f38e
Mariusz <mariusz.lepkowski23@gmail.com>
2024-02-02 20:28:29 +0100

    Change project configuration inside main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+
+from VC_music_player import create_app as create_music_app
 from VC_discography_scraper.discography_downloader import DiscographyDownloader
+from VC_music_player.views.url import url_blueprint
+from VC_music_player.views.audio_player import audio_player_blueprint
 
 DISCOGRAPHY_URL = "https://vinniecolaiuta.com/Home/GetPageOfRecordings?page="
 
+VC_music_player_app = create_music_app()
+VC_music_player_app.register_blueprint(url_blueprint)
+VC_music_player_app.register_blueprint(audio_player_blueprint)
 
-def main():
-    discography_downloader = DiscographyDownloader(DISCOGRAPHY_URL)
-
-    discography_downloader.pick_random_album()
-
+# Run scraper
+discography_downloader = DiscographyDownloader(DISCOGRAPHY_URL)
+discography_downloader.pick_random_album()
 
 if __name__ == "__main__":
-    main()
+    VC_music_player_app.run(debug=True, port=5000)
