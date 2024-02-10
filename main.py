ce12e3312482b66961f56a1311595b61789977ac
Mariusz <mariusz.lepkowski23@gmail.com>
2024-02-10 20:37:47 +0100

    Delete Url view

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from VC_Radio_app import create_app
-from VC_Radio_app.views.url import url_blueprint
 from VC_Radio_app.views.audio_player import audio_player_blueprint
 from VC_Radio_app.views.search_album import search_album_blueprint
 
 
-
 VC_Radio_app = create_app()
-VC_Radio_app.register_blueprint(url_blueprint)
 VC_Radio_app.register_blueprint(audio_player_blueprint)
 VC_Radio_app.register_blueprint(search_album_blueprint)
 
