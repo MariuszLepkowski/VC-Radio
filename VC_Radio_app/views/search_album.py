a91b71944083558a7e1d11ca384884f519a5fad0
Mariusz <mariusz.lepkowski23@gmail.com>
2024-02-11 20:34:44 +0100

    Hide search results before picking random album

--- VC_Radio_app/views/search_album.py
+++ VC_Radio_app/views/search_album.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from VC_discography_scraper.discography_downloader import DiscographyDownloader
 from VC_Radio_app.yt_search import search_album_on_ytmusic, search_playlist_on_ytmusic, search_video_on_yt, search_playlist_on_yt
 
@@ -10,38 +10,36 @@ search_album_blueprint = Blueprint('search_album', __name__)
 
 discography_downloader = DiscographyDownloader(DISCOGRAPHY_URL)
 
+search_results_info = {
+    "yt_music_album": None,
+    "yt_music_playlist": None,
+    "yt_video": None,
+    "yt_playlist": None,
+}
 
 @search_album_blueprint.route('/search', methods=['GET', 'POST'])
 def search_album():
-    print("Downloading and saving discography...")
+    if request.method == 'POST':
+        print("Downloading and saving discography...")
 
-    picked_album = discography_downloader.pick_random_album()
+        picked_album = discography_downloader.pick_random_album()
 
-    search_results_info = {
-        "yt_music_album": '',
-        "yt_music_playlist": '',
-        "yt_video": '',
-        "yt_playlist": '',
-    }
+        search_query = picked_album['artist'] + ' ' + picked_album['album_title']
 
-    artist_query = picked_album['artist']
-    title_query = picked_album['album_title']
+        try:
+            album_found = search_album_on_ytmusic(search_query)
+            ytm_playlist_found = search_playlist_on_ytmusic(search_query)
+            video_found = search_video_on_yt(search_query)
+            yt_playlist_found = search_playlist_on_yt(search_query)
 
-    search_query = artist_query + ' ' + title_query
+            search_results_info["yt_music_album"] = album_found
+            search_results_info["yt_music_playlist"] = ytm_playlist_found
+            search_results_info["yt_video"] = video_found
+            search_results_info["yt_playlist"] = yt_playlist_found
 
-    try:
-        album_found = search_album_on_ytmusic(search_query)
-        ytm_playlist_found = search_playlist_on_ytmusic(search_query)
-        video_found = search_video_on_yt(search_query)
-        yt_playlist_found = search_playlist_on_yt(search_query)
+        except IndexError:
+            print("An error occurred.")
 
+        return render_template('search-album.html', album=picked_album, results=search_results_info)
 
-        search_results_info["yt_music_album"] = album_found
-        search_results_info["yt_music_playlist"] = ytm_playlist_found
-        search_results_info["yt_video"] = video_found
-        search_results_info["yt_playlist"] = yt_playlist_found
-
-    except IndexError:
-        print("An error occurred.")
-
-    return render_template('search-album.html', album=picked_album, results=search_results_info)
+    return render_template('search-album.html', album=None, results=search_results_info)
