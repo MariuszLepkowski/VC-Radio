72a8fb1fd3dfbff8bf1d420b336f43ab54c88ee2
Mariusz <mariusz.lepkowski23@gmail.com>
2024-02-13 14:35:02 +0100

    Create home.html with homepage layer view

--- VC_Radio_app/views/audio_player.py
+++ VC_Radio_app/views/audio_player.py
@@ -6,5 +6,5 @@ audio_player_blueprint = Blueprint('audio_player', __name__)
 @audio_player_blueprint.route('/player')
 def audio_player():
     video_id = session.get('video_id')
-    return render_template('audio-player.html', video_id=video_id)
+    return render_template('home.html', video_id=video_id)
 
