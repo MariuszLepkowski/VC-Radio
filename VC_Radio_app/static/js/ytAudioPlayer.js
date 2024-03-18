a34c74d1824f7789f6f92a58493d0c94723f19bc
Mariusz <mariusz.lepkowski23@gmail.com>
2024-03-18 19:40:56 +0100

    BUG to fix: UI does not refresh after alternative link is shown.

--- VC_Radio_app/static/js/ytAudioPlayer.js
+++ VC_Radio_app/static/js/ytAudioPlayer.js
@@ -60,24 +60,19 @@ function onYouTubeIframeAPIReady() {
 function startTitleChecking() {
     var titleCheckInterval = setInterval(function() {
         var titleElement = document.getElementById('title');
+        console.log("Title Element:", titleElement);
+        console.log("Title Content:", titleElement.textContent.trim());
+        console.log("Condition:", titleElement && titleElement.textContent.trim() !== "");
         if (titleElement && titleElement.textContent.trim() !== "") {
             clearInterval(titleCheckInterval);
         } else {
-            showAlternativeLink(document.getElementById("youtube-audio").dataset.video);
+            // Sprawdź, czy audio jest odtwarzane
+            if (!isPlaying) {
+                console.log("Show Alternative Link!");
+                showAlternativeLink(document.getElementById("youtube-audio").dataset.video);
+            }
         }
     }, 1000);
-}
-
-
-
-// Function to check if an element has a title
-function hasTitle(element) {
-    if (!element) {
-        return false;
-    }
-
-    var titleElement = element.querySelector('#title');
-    return titleElement && titleElement.textContent.trim() !== "";
 }
 
 
@@ -103,40 +98,6 @@ function updateTimeInfo() {
 
 
 
-// Function to update the UI
-function updateUI() {
-    var durationElement = document.getElementById('timeInfo');
-    var progressBar = document.getElementById('progressBar');
-    var progressThumb = document.getElementById('progressThumb');
-    var progressContainer = document.getElementById('progressContainer');
-    var progressPercentage = (currentTime / duration) * 100;
-    progressBar.style.width = progressPercentage + '%';
-    progressThumb.style.left = (progressPercentage - 1) + '%';
-
-    var youtubeIcon = document.getElementById('youtube-icon');
-    youtubeIcon.style.backgroundImage = 'url(' + (isPlaying ? '/static/assets/img/pause.png' : '/static/assets/img/play.png') + ')';
-
-    var formattedTime = formatTime(currentTime) + ' / ' + formatTime(duration);
-    durationElement.textContent = formattedTime;
-
-    updateTitle();
-
-
-}
-
-
-
-// Function to update the video title
-function updateTitle() {
-    var currentTitleElement = document.getElementById('title');
-    var videoData = player.getVideoData();
-    var title = videoData.title;
-
-    currentTitleElement.textContent = title;
-}
-
-
-
 // Function to format time
 function formatTime(time) {
     var minutes = Math.floor(time / 60);
@@ -156,12 +117,51 @@ function seekToTime(event) {
 }
 
 
+// Function to update the video title
+function updateTitle() {
+    var currentTitleElement = document.getElementById('title');
+    var videoData = player.getVideoData();
+    var title = videoData.title;
+
+    currentTitleElement.textContent = title;
+}
+
+// Function to update the UI
+function updateUI() {
+    var durationElement = document.getElementById('timeInfo');
+    var progressBar = document.getElementById('progressBar');
+    var progressThumb = document.getElementById('progressThumb');
+    var progressContainer = document.getElementById('progressContainer');
+    var progressPercentage = (currentTime / duration) * 100;
+    progressBar.style.width = progressPercentage + '%';
+    progressThumb.style.left = (progressPercentage - 1) + '%';
+
+    var youtubeIcon = document.getElementById('youtube-icon');
+    youtubeIcon.style.backgroundImage = 'url(' + (isPlaying ? '/static/assets/img/pause.png' : '/static/assets/img/play.png') + ')';
+
+    var formattedTime = formatTime(currentTime) + ' / ' + formatTime(duration);
+    durationElement.textContent = formattedTime;
+
+    updateTitle();
+}
+
+
+// Function to display an alternative link
 function showAlternativeLink(videoId) {
-  var alternativeLinkDiv = document.getElementById('alternative-link');
-  if (alternativeLinkDiv) {
-    alternativeLinkDiv.style.display = 'block';
-    alternativeLinkDiv.innerHTML = '<p>The audio/video available only on <a href="https://www.youtube.com/watch?v=' + videoId + '" target="_blank"><img id="youtube-logo" src="static/assets/img/YouTubeLogo.png" alt="YouTube Logo"> <br> Click to listen/watch.</a></p>';
-  }
+    var alternativeLinkDiv = document.getElementById('alternative-link');
+    var playerContainer = document.getElementById('container');
+    var loadingDiv = document.getElementById('title');
+
+    // Sprawdź, czy tytuł jest już dostępny
+    if (loadingDiv.style.display !== 'none') {
+        // Jeśli nie, wyświetl link alternatywny
+        alternativeLinkDiv.style.display = 'block';
+        alternativeLinkDiv.innerHTML = '<p>The audio/video available only on <a href="https://www.youtube.com/watch?v=' + videoId + '" target="_blank"><img id="youtube-logo" src="static/assets/img/YouTubeLogo.png" alt="YouTube Logo"> <br> Click to listen/watch.</a></p>';
+
+        // Ukryj resztę odtwarzacza
+        playerContainer.style.display = 'none';
+        loadingDiv.style.display = 'none';
+    }
 }
 
 
@@ -183,7 +183,9 @@ function updateHTMLView(videoId) {
 
     isPlaying = true;
 
-    showAlternativeLink(videoId);
+//    setTimeout(function() {
+//        showAlternativeLink(videoId);
+//    }, 4000);
 }
 
 
