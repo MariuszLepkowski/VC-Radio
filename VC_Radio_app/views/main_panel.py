8daff15db003a28717f044327dcb56eef10872f3
Mariusz <mariusz.lepkowski23@gmail.com>
2024-03-21 14:49:21 +0100

    Show Generate album button inside Album generator section

--- VC_Radio_app/views/main_panel.py
+++ VC_Radio_app/views/main_panel.py
@@ -5,21 +5,23 @@ from VC_Radio_app.utils.search_album_utils import search_album
 main_panel_blueprint = Blueprint('main_panel', __name__)
 
 
-@main_panel_blueprint.route('/')
-def main_panel():
-    return render_template('main-panel.html')
+def process_album_request():
+    if request.method == 'POST':  # Jeśli formularz został wysłany
+        return search_album()
+    else:  # Jeśli jest to pierwsze wyświetlenie strony
+        return None, None
 
+
+@main_panel_blueprint.route('/', methods=['GET', 'POST'])
+def main_panel():
+    album, results = process_album_request()
+    return render_template('main-panel.html', album=album, results=results)
+
+@main_panel_blueprint.route('/album-generator', methods=['GET', 'POST'])
+def album_generator():
+    album, results = process_album_request()
+    return render_template('album-generator.html', album=album, results=results)
 
 @main_panel_blueprint.route('/about')
 def about():
     return render_template('about.html')
-
-
-@main_panel_blueprint.route('/album-generator', methods=['GET', 'POST'])
-def album_generator():
-    if request.method == 'POST':
-        album, results = search_album()
-    else:
-        album = None
-        results = None
-    return render_template('album-generator.html', album=album, results=results)
