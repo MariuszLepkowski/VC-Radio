d5577d3c54f8984f4d79c5dc3ff00c80f1fb9bf5
Mariusz <mariusz.lepkowski23@gmail.com>
2024-04-28 23:09:49 +0200

    Create search_album_track_on_yt() to search for an album on regular YT by filtering channels with 'Topic' phrase. Delete ytmusicapi library from requirements

--- VC_Radio_app/utils/search_album_utils.py
+++ VC_Radio_app/utils/search_album_utils.py
@@ -1,5 +1,5 @@
 from VC_discography_scraper.discography_downloader import DiscographyDownloader
-from VC_Radio_app.utils.yt_search_utils import search_album_on_ytmusic, search_playlist_on_ytmusic, search_video_on_yt, search_playlist_on_yt
+from VC_Radio_app.utils.yt_search_utils import search_video_on_yt, search_playlist_on_yt, search_album_track_on_yt
 
 discography_downloader = DiscographyDownloader("https://vinniecolaiuta.com/Home/GetPageOfRecordings?page=")
 
@@ -8,23 +8,23 @@ def search_album():
     picked_album = discography_downloader.pick_random_album()
     search_query = picked_album['artist'] + ' ' + picked_album['album_title']
     try:
-        album_found = search_album_on_ytmusic(search_query)
-        ytm_playlist_found = search_playlist_on_ytmusic(search_query)
+        album_track_found = search_album_track_on_yt(search_query)
         video_found = search_video_on_yt(search_query)
         yt_playlist_found = search_playlist_on_yt(search_query)
+        # ytm_playlist_found = search_playlist_on_ytmusic(search_query)
 
         search_results_info = {
-            "yt_music_album": album_found,
-            "yt_music_playlist": ytm_playlist_found,
+            "yt_album_track": album_track_found,
+            "yt_playlist": yt_playlist_found,
             "yt_video": video_found,
-            "yt_playlist": yt_playlist_found
+            # "yt_music_playlist": ytm_playlist_found,
         }
     except IndexError:
         print("An error occurred.")
         search_results_info = {
-            "yt_music_album": None,
-            "yt_music_playlist": None,
+            "yt_album_tracks": None,
+            "yt_playlist": None,
             "yt_video": None,
-            "yt_playlist": None
+            # "yt_music_playlist": None,
         }
     return picked_album, search_results_info
--- VC_Radio_app/utils/yt_search_utils.py
+++ VC_Radio_app/utils/yt_search_utils.py
@@ -13,6 +13,49 @@ youtube = build("youtube", "v3", developerKey=YT_API_KEY)
 yt = YTMusic('oauth.json')
 
 
+def search_album_track_on_yt(search_query):
+    """Searches for an album on regular YT by filtering channels with 'Topic' phrase"""
+    print('search_video_on_yt')
+
+    album_track_found = False
+    print(f'searchquery={search_query}')
+
+    # artist_query, title_query = search_query.split(' ', 1)
+
+    request = youtube.search().list(
+        part='snippet',
+        q=search_query,
+        type='video',
+        maxResults=50,
+    )
+    response = request.execute()
+
+    yt_video_info = []
+
+    for item in response['items']:
+
+        if "Topic" in item['snippet']['channelTitle'] and title_query in item['snippet']['description']:
+            print(item['snippet']['channelTitle'])
+            print(title_query)
+            print(item['snippet']['description'])
+            video_id = item['id']['videoId']
+            video_title = item['snippet']['title']
+
+            yt_video_info.append(
+                {
+                'YouTubeVideoTitle': video_title,
+                'YouTubeVideoUrl': YT_VIDEO_ENDPOINT + video_id,
+                'video_id': video_id,
+                }
+            )
+
+            album_track_found = True
+        else:
+            pass
+
+    return album_track_found, yt_video_info
+
+
 def search_playlist_on_yt(search_query):
     """Searches for playlists on regular YT"""
     print('search_playlist_on_yt')
@@ -77,120 +120,6 @@ def search_playlist_on_yt(search_query):
     return yt_playlist_found, yt_playlist_info
 
 
-def search_album_on_ytmusic(search_query):
-    """Searches for each individual track on the requested album on YT Music"""
-
-    print('search_album_on_ytmusic')
-
-    album_found = False
-
-    artist_query, title_query = search_query.split(' ', 1)
-
-    search_results = yt.search(query=search_query, filter='albums')
-
-    ytmusic_album_info = []
-
-    unique_album_titles = set()
-
-    for result in search_results:
-        album_artist = result['artists'][1]['name']
-        album_title = result['title']
-
-        if all(word.lower() in album_artist.lower() for word in artist_query.lower().split()) or all(
-                word in album_title.lower().split() for word in title_query.lower().split()):
-
-            if album_title not in unique_album_titles:
-                album_found = True
-                unique_album_titles.add(album_title)
-
-                album_browseId = result['browseId']
-
-                try:
-                    album_info = yt.get_album(browseId=album_browseId)
-                except ValueError as e:
-                    print(f"Error while processing album: {e}")
-                    continue
-
-                album_tracklist = album_info['tracks']
-
-                album_data = {
-                    'ytMusicAlbumTitle': album_title,
-                    'ytMusicAlbumArtist': album_artist,
-                    'AlbumTracklist': []
-                }
-
-                for track in album_tracklist:
-                    album_data['AlbumTracklist'].append(
-                        {
-                            'trackNumber': track['trackNumber'],
-                            'trackTitle': track['title'],
-                            'trackURL': YT_VIDEO_ENDPOINT + track['videoId'] if track.get('videoId') else "",
-                            'video_id': track['videoId'] if track.get('videoId') else "",
-                        }
-                    )
-
-                ytmusic_album_info.append(album_data)
-
-    return album_found, ytmusic_album_info
-
-
-def search_playlist_on_ytmusic(search_query):
-    """Searches for playlists on YT Music"""
-    print('search_playlist_on_ytmusic')
-
-    ytm_playlist_found = False
-
-    artist_query, title_query = search_query.split(' ', 1)
-
-    search_results = yt.search(search_query, filter='playlists')
-
-    ytmusic_playlist_info = []
-
-    for result in search_results:
-        playlist_title_wordlist = result['title'].lower().split()
-
-        if all(word in playlist_title_wordlist for word in artist_query.lower().split()) or all(
-                word in playlist_title_wordlist for word in title_query.lower().split()):
-            ytm_playlist_found = True
-
-            try:
-                playlist_info = yt.get_playlist(playlistId=result['browseId'])
-            except ValueError as e:
-                print(f"Error while processing playlist: {e}")
-                continue
-
-            playlist_tracklist = playlist_info.get('tracks', [])
-            playlist_tracks = []
-
-            track_num = 0
-
-            for track in playlist_tracklist:
-                track_num += 1
-                track_title = track['title']
-                if 'videoId' in track and track['videoId'] is not None:
-                    track_url = YT_VIDEO_ENDPOINT + track['videoId']
-                    playlist_tracks.append(
-                        {
-                            'trackNumber': track_num,
-                            'trackTitle': track_title,
-                            'trackURL': track_url,
-                            'video_id': track['videoId'],
-                        }
-                    )
-                else:
-                    print("Warning: Missing or invalid videoId for track. Skipping...")
-
-            ytmusic_playlist_info.append(
-                {
-                    'ytMusicPlaylistTitle': result['title'],
-                    'ytMusicPlaylistURL': YT_PLAYLIST_ENDPOINT + playlist_info['id'],
-                    'PlaylistTracklist': playlist_tracks,
-                }
-            )
-
-    return ytm_playlist_found, ytmusic_playlist_info
-
-
 def search_video_on_yt(search_query):
     """Searches for a video on regular YT"""
     print('search_video_on_yt')
@@ -199,7 +128,12 @@ def search_video_on_yt(search_query):
 
     artist_query, title_query = search_query.split(' ', 1)
 
-    request = youtube.search().list(part='snippet', q=search_query, type='video')
+    request = youtube.search().list(
+        part='snippet',
+        q=search_query,
+        type='video',
+        maxResults=50,
+    )
     response = request.execute()
 
     yt_video_info = []
@@ -219,3 +153,117 @@ def search_video_on_yt(search_query):
         video_found = True
 
     return video_found, yt_video_info
+
+
+# def search_album_on_ytmusic(search_query):
+#     """Searches for each individual track on the requested album on YT Music"""
+#
+#     print('search_album_on_ytmusic')
+#
+#     album_found = False
+#
+#     artist_query, title_query = search_query.split(' ', 1)
+#
+#     search_results = yt.search(query=search_query, filter='albums')
+#
+#     ytmusic_album_info = []
+#
+#     unique_album_titles = set()
+#
+#     for result in search_results:
+#         album_artist = result['artists'][1]['name']
+#         album_title = result['title']
+#
+#         if all(word.lower() in album_artist.lower() for word in artist_query.lower().split()) or all(
+#                 word in album_title.lower().split() for word in title_query.lower().split()):
+#
+#             if album_title not in unique_album_titles:
+#                 album_found = True
+#                 unique_album_titles.add(album_title)
+#
+#                 album_browseId = result['browseId']
+#
+#                 try:
+#                     album_info = yt.get_album(browseId=album_browseId)
+#                 except ValueError as e:
+#                     print(f"Error while processing album: {e}")
+#                     continue
+#
+#                 album_tracklist = album_info['tracks']
+#
+#                 album_data = {
+#                     'ytMusicAlbumTitle': album_title,
+#                     'ytMusicAlbumArtist': album_artist,
+#                     'AlbumTracklist': []
+#                 }
+#
+#                 for track in album_tracklist:
+#                     album_data['AlbumTracklist'].append(
+#                         {
+#                             'trackNumber': track['trackNumber'],
+#                             'trackTitle': track['title'],
+#                             'trackURL': YT_VIDEO_ENDPOINT + track['videoId'] if track.get('videoId') else "",
+#                             'video_id': track['videoId'] if track.get('videoId') else "",
+#                         }
+#                     )
+#
+#                 ytmusic_album_info.append(album_data)
+#
+#     return album_found, ytmusic_album_info
+#
+#
+# def search_playlist_on_ytmusic(search_query):
+#     """Searches for playlists on YT Music"""
+#     print('search_playlist_on_ytmusic')
+#
+#     ytm_playlist_found = False
+#
+#     artist_query, title_query = search_query.split(' ', 1)
+#
+#     search_results = yt.search(search_query, filter='playlists')
+#
+#     ytmusic_playlist_info = []
+#
+#     for result in search_results:
+#         playlist_title_wordlist = result['title'].lower().split()
+#
+#         if all(word in playlist_title_wordlist for word in artist_query.lower().split()) or all(
+#                 word in playlist_title_wordlist for word in title_query.lower().split()):
+#             ytm_playlist_found = True
+#
+#             try:
+#                 playlist_info = yt.get_playlist(playlistId=result['browseId'])
+#             except ValueError as e:
+#                 print(f"Error while processing playlist: {e}")
+#                 continue
+#
+#             playlist_tracklist = playlist_info.get('tracks', [])
+#             playlist_tracks = []
+#
+#             track_num = 0
+#
+#             for track in playlist_tracklist:
+#                 track_num += 1
+#                 track_title = track['title']
+#                 if 'videoId' in track and track['videoId'] is not None:
+#                     track_url = YT_VIDEO_ENDPOINT + track['videoId']
+#                     playlist_tracks.append(
+#                         {
+#                             'trackNumber': track_num,
+#                             'trackTitle': track_title,
+#                             'trackURL': track_url,
+#                             'video_id': track['videoId'],
+#                         }
+#                     )
+#                 else:
+#                     print("Warning: Missing or invalid videoId for track. Skipping...")
+#
+#             ytmusic_playlist_info.append(
+#                 {
+#                     'ytMusicPlaylistTitle': result['title'],
+#                     'ytMusicPlaylistURL': YT_PLAYLIST_ENDPOINT + playlist_info['id'],
+#                     'PlaylistTracklist': playlist_tracks,
+#                 }
+#             )
+#
+#     return ytm_playlist_found, ytmusic_playlist_info
